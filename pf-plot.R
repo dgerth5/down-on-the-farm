e2261866d194a6fba4c7f5426ac138e6ac477e56
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2023-03-02 22:04:03 -0600

    Update pf-plot.R

--- pf-plot.R
+++ pf-plot.R
@@ -4,21 +4,6 @@ milb_park_effects22 <- read_csv("milb_park_effects22.csv")
 library(gt)
 library(tidyverse)
 
-theme_scott <- function () {
-  
-  theme_minimal(base_size=9) %+replace%
-    theme(
-      panel.grid.minor = element_blank(),
-      plot.background = element_rect(
-        fill = "aliceblue", color = "aliceblue"
-      ),
-      axis.text.y = element_text(size = 10),
-      axis.text.x = element_text(size = 10),
-      plot.margin = margin(10, 10, 20, 10) 
-    )
-  
-}
-
 just_hr <- milb_park_effects22 %>%
   select(team, home_run_pf, home_run_mult) %>%
   arrange(-home_run_pf) %>%
