ea1909a8473e935c80a6f97817c677da8a26fb93
dgerth5 <davidgerth5@gmail.com>
2023-04-26 23:39:23 -0500

    ideas

--- tto-stabilization.R
+++ tto-stabilization.R
@@ -32,6 +32,32 @@ ml_pbp <- 1:length(ml_game_pks) %>%
 
 # get season stats
 
+
+# get batter team name 
+finished_ab <- ml_pbp %>%
+  group_by(game_pk) %>%
+  arrange(atBatIndex, .by_group = TRUE) %>%
+  mutate(ld = lead(matchup.batter.id, 1),
+         last_pitch_ab = if_else(is.na(ld) == TRUE, 1, 
+                                 if_else(matchup.batter.id != ld, 1, 0))) %>%
+  ungroup() %>%
+  filter(last_pitch_ab == 1)
+
+# add batter team name
+agg_sts <- finished_ab %>%
+  mutate(strikeout = if_else(result.eventType == "strikeout" | result.eventType == "strikeout_double_play", 1, 0),
+         walk = if_else(result.eventType == "walk", 1, 0),
+         home_run = if_else(result.eventType == "home_run", 1, 0),
+         adj_name = paste0(matchup.batter.fullName, matchup.batter.id)) %>%
+  group_by(adj_name,home_level_name) %>%
+  summarise(abs = n(),
+            tot_bbs = sum(walk),
+            tot_ks = sum(strikeout),
+            tot_hrs = sum(home_run))
+
+
+
+
 # apply stabilization method
 # copying from post: https://downonthefarm.substack.com/p/estimating-minor-league-stabilization
 
