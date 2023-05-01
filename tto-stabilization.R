fa6688872fb16f86fba63ae3c1a20b59923b64ab
dgerth5 <davidgerth5@gmail.com>
2023-04-30 23:12:42 -0500

    upto4-30

--- tto-stabilization.R
+++ tto-stabilization.R
@@ -21,7 +21,7 @@ ml_game_pks <- minor_league_game_pk_lst %>%
                 !is.na(game_pk)) %>%
   pull(game_pk)
 
-plan("multisession", workers = 3)
+plan("multisession", workers = 4)
 
 safe_pbp <- safely(mlb_pbp)
 
@@ -30,18 +30,34 @@ ml_pbp <- 1:length(ml_game_pks) %>%
   purrr::map("result") %>%
   bind_rows()
 
+library(readr)
+
+write_csv(ml_pbp, "milb2023-4-30.csv")
+
+ml_pbp <- read_csv("milb2023-4-30.csv")
+
+
 # get season stats
 
 
+
 # get batter team name 
+# these filter out the annoying things that end an event but not a plate appearance
+bad_events <- c("Pickoff Caught Stealing 2B","Runner Out","Caught Stealing 3B","Catcher Interference",
+                "Wild Pitch","Pickoff 1B","Caught Stealing 2B","Pickoff Caught Stealing 3B",
+                "Caught Stealing Home","Pickoff 3B","Stolen Base 3B","Pickoff Caught Stealing Home",
+                "Pickoff 2B")
+
 finished_ab <- ml_pbp %>%
+  filter(isPitch == TRUE) %>%
   group_by(game_pk) %>%
-  arrange(atBatIndex, .by_group = TRUE) %>%
+  arrange(atBatIndex, pitchNumber, .by_group = TRUE) %>%
   mutate(ld = lead(matchup.batter.id, 1),
          last_pitch_ab = if_else(is.na(ld) == TRUE, 1, 
-                                 if_else(matchup.batter.id != ld, 1, 0))) %>%
-  ungroup() %>%
-  filter(last_pitch_ab == 1)
+                                 if_else(matchup.batter.id != ld, 1, 0)))  %>%
+  filter(last_pitch_ab == 1) %>%
+  filter(!result.event %in% bad_events) %>%
+  ungroup()
 
 # add batter team name
 agg_sts <- finished_ab %>%
@@ -49,13 +65,11 @@ agg_sts <- finished_ab %>%
          walk = if_else(result.eventType == "walk", 1, 0),
          home_run = if_else(result.eventType == "home_run", 1, 0),
          adj_name = paste0(matchup.batter.fullName, matchup.batter.id)) %>%
-  group_by(adj_name,home_level_name) %>%
-  summarise(abs = n(),
+  group_by(adj_name,home_level_name, batting_team) %>%
+  summarise(pas = sum(last_pitch_ab),
             tot_bbs = sum(walk),
             tot_ks = sum(strikeout),
             tot_hrs = sum(home_run))
-
-
 
 
 # apply stabilization method
