eced21666cafdc1052f8a7dc688db9fd599bef03
dgerth5 <davidgerth5@gmail.com>
2023-05-04 00:26:37 -0500

    slight adjustments

--- adjust-40260ft.R
+++ adjust-40260ft.R
@@ -26,24 +26,27 @@ ml_pbp <- 1:length(ml_game_pks) %>%
 
 ml_pbp <- ml_pbp %>% as.data.frame()
 
+readr::write_csv(ml_pbp, "mlb2022-40ft.csv")
+
 just_pitches <- ml_pbp %>% filter(isPitch == TRUE)
 
 just2 <- just_pitches %>% 
-  select(game_date, matchup.pitcher.id, matchup.batter.id, details.type.code, pitchData.startSpeed, about.atBatIndex, pitchNumber, pitchData.breaks.spinRate) %>%
+  select(game_date, matchup.pitcher.id, matchup.batter.id, details.type.code, pitchData.startSpeed, about.atBatIndex, pitchNumber, pitchData.coordinates.pfxZ) %>%
   mutate(atBatIndex2 = about.atBatIndex + 1,
          concat = paste0(game_date, matchup.pitcher.id, matchup.batter.id, details.type.code, pitchData.startSpeed, atBatIndex2	, pitchNumber))
 
 statcast22 <- read_csv("C:/Users/david/PycharmProjects/pythonProject/statcast22.csv")
 
 statcast2 <- statcast22 %>%
-  select(game_date, pitcher, batter, pitch_type, release_speed, at_bat_number, pitch_number, release_spin_rate) %>%
+  select(game_date, pitcher, batter, pitch_type, release_speed, at_bat_number, pitch_number, pfx_z) %>%
   mutate(concat = paste0(game_date, pitcher, batter, pitch_type, release_speed, at_bat_number, pitch_number))
 
 master <- inner_join(statcast2, just2, by = "concat") %>%
   drop_na()
 
 master2 <- master %>%
-  select(release_spin_rate, pitchData.breaks.spinRate, release_speed)
+  mutate(adj_x = pfx_z*12) %>%
+  select(adj_x,pitchData.coordinates.pfxZ,release_speed)
 
-mod <- lm(release_spin_rate ~ pitchData.breaks.spinRate + release_speed, data = master2)
+mod <- lm(adj_x ~ pitchData.coordinates.pfxZ + release_speed, data = master2)
 summary(mod)
